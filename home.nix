863d2b2727a464e9571699b6d0eaf74608debb72
benedikt <benedikt.theuretzbachner@gmail.com>
2025-12-04 07:59:00 +0100

    home: add brightnessctl

--- home.nix
+++ home.nix
@@ -31,6 +31,8 @@
     wl-clipboard
     anki-bin
 
+    brightnessctl
+
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
