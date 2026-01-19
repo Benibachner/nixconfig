853eaff071a332558a3f12be8087e529f23f73a5
benedikt <benedikt.theuretzbachner@gmail.com>
2026-01-19 11:03:12 +0100

    home: add terraform

--- home.nix
+++ home.nix
@@ -40,6 +40,7 @@
     obsidian
     rawtherapee
     libreoffice
+    terraform
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
