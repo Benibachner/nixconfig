46abe4791648b4a0b820b47c9dea4ff15862763d
benedikt <benedikt.theuretzbachner@gmail.com>
2026-01-19 12:18:19 +0100

    neovim: configure lazyvim, typst lsp, keymaps

--- terminal/neovim.nix
+++ terminal/neovim.nix
@@ -18,9 +18,33 @@
       };
       lang.rust = {
         enable = true;
-        installDependencies = true;        # Install gopls, gofumpt, etc.
-        installRuntimeDependencies = true; # Install go compiler
+        installDependencies = true;
+        installRuntimeDependencies = true;
       };
+      lang.typst = {
+        enable = true;
+        installDependencies = true;
+        installRuntimeDependencies = true;
+      };
+    };
+
+    config = {
+      options = ''
+        vim.opt.relativenumber = false
+        vim.opt.wrap = true
+
+        vim.cmd("highlight Normal guibg=none")
+        vim.cmd("highlight NonText guibg=none")
+        vim.cmd("highlight Normal ctermbg=none")
+        vim.cmd("highlight NonText ctermbg=none")
+      '';
+
+      keymaps = ''
+        vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Save" })
+        vim.keymap.set("i", "vj", "<Esc>", { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+        vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+      '';
     };
 
     # Additional packages (optional)
